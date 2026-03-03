22cf02ca6be5631d1414aaeba493fb408bee2c31
Mostafa Noah <92794284+MostafaNouh0011@users.noreply.github.com>
2026-03-03 23:53:03 +0200

    Update bronze_ddl.sql

--- scripts/bronze_layer/bronze_ddl.sql
+++ scripts/bronze_layer/bronze_ddl.sql
@@ -78,7 +78,7 @@ CREATE TABLE bronze.crm_sales_details (
     sls_due_dt      INT,
     sls_sales       INT,
     sls_quantity    INT,
-    sls_price       VARCHAR(50)
+    sls_price       INT
 );
 GO
 
