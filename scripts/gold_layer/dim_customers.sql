e78ed7fbfbd0f60db4eff7507284d28d2c65910d
Mostafa Noah <92794284+MostafaNouh0011@users.noreply.github.com>
2026-03-14 22:05:31 +0200

    Update dim_customers.sql

---
+++ scripts/gold_layer/dim_customers.sql
@@ -0,0 +1,45 @@
+/*
+====================================================================================
+Gold Layer - Dimension: Customers
+====================================================================================
+Description:
+    Creates the Customer dimension view by integrating CRM and ERP customer data.
+    A surrogate key is generated using ROW_NUMBER().
+====================================================================================
+*/
+
+CREATE OR ALTER VIEW gold.dim_customers AS
+
+SELECT
+
+    -- Surrogate Key
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+
+    -- Customer Identifiers
+    ci.cst_id             AS customer_id,
+    ci.cst_key            AS customer_number,
+
+    -- Customer Personal Information
+    ci.cst_firstname      AS first_name,
+    ci.cst_lastname       AS last_name,
+    ca.bdate              AS birthdate,
+
+    -- Customer Demographics
+    la.cntry              AS country,
+    ci.cst_marital_status AS marital_status,
+
+    -- Gender Resolution Logic
+    CASE
+        WHEN ci.cst_gndr = 'n/a'
+            THEN COALESCE(ca.gen, 'n/a')
+        ELSE ci.cst_gndr                  -- CRM is the master source for gender
+    END AS gender,
+
+    -- Metadata
+    ci.cst_create_date   AS create_date
+
+FROM silver.crm_cust_info      AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+        ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101  AS la
+        ON ci.cst_key = la.cid;
